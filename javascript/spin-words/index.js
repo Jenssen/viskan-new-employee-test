80deb379ce37ee9af23aaab06af0350dc289ca24
Robert Jenssen <robert@jenssen.xyz>
2024-05-12 23:14:39 +0200

    Javascript - Spin words done

--- javascript/spin-words/index.js
+++ javascript/spin-words/index.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = input => {
-	return input;
+	/*
+		Split string into an array and then reverse all words with more than 5 letters.
+		Convert back to string again to match test spec
+	*/
+	const array = input.split(' ').map((word) => {
+	  if (word.length > 5) {
+	    return word.split('').reverse().join('')
+	  }
+	  return word
+	})
+
+	const final = array.join(' ')
+	return final
 };
