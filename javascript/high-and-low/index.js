74fa7ab5e65b2e77341cc758749e7dadadc9176c
Robert Jenssen <robert@jenssen.xyz>
2024-05-12 22:58:19 +0200

    Javascript - High and low done

--- javascript/high-and-low/index.js
+++ javascript/high-and-low/index.js
@@ -1,4 +1,16 @@
 'use strict';
 module.exports = input => {
-	return input;
+	/*
+		Split string to array and then use built in functions to find max and min value.
+		Return a string to match test spec.
+	*/
+	const array = input.split(' ').map((number) => {
+	  return parseInt(number)
+	})
+
+	const max = Math.max(...array)
+	const min = Math.min(...array)
+
+	const final = [max, min].join(' ')
+	return final;
 };
