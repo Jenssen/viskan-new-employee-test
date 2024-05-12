b7ff7896a06dc3b40d4a53f6bae74b2dc66da70a
Robert Jenssen <robert@jenssen.xyz>
2024-05-12 21:53:04 +0200

    Web API - Task 1 done

---
+++ web-api/task-1/index.js
@@ -0,0 +1,14 @@
+const button = document.querySelector('button')
+
+/*
+  Save current time in localStorage on click.
+  Use object and JSON so it's easier to add more data if needed
+*/
+button.addEventListener('click', () => {
+  const data = {
+    timestamp: Date.now()
+  }
+
+  localStorage.setItem('timestamp', JSON.stringify(data))
+  console.log('Current time saved to localStorage');
+})
